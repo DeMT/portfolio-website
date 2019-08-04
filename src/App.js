4552c1749b8f8ae3621fe98f7de74ea2dfca0512
DeMT <gn00468461@gmail.com>
2019-08-05 01:17:15 +0800

    second commit, finish the shop page, preview.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Homepage from './pages/homepage.component'
 import './App.css';
-
+import {Route} from 'react-router-dom'
+import Shop from './pages/shop/shop.component'
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Route exact path = "/shop" component = {Shop} />
+      <Route exact path = "/" component = {Homepage} />
     </div>
   );
 }
--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,9 +1,8 @@
 import React from 'react' 
 import Directory from '../component/directory/directory.component'
-
 export default function Homepage() {
     return(
-    <div className = 'homepage'>
+    <div className = 'homepage'>        
         <Directory />
     </div>
     )
