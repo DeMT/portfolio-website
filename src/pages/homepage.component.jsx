b0eaf47f1e6298f6d750da8c042302a69ab363ce
DeMT <gn00468461@gmail.com>
2019-08-04 12:40:35 +0800

    First commit, include homepage and menu-item.

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react' 
+import Directory from '../component/directory/directory.component'
+
+export default function Homepage() {
+    return(
+    <div className = 'homepage'>
+        <Directory />
+    </div>
+    )
+
+
+
+}
